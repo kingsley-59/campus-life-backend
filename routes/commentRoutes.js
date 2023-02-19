0c9f87180f7d8c865392d4375d09375eeeca81fc
Kizito007 <nwakakelechi49@gmail.com>
2023-02-19 22:50:35 +0100

    added auth to comment

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const commentRoute = express.Router();
+const { auth } = require('../middleware/auth');
 const commentController = require('../controllers/commentController');
 const replyController = require('../controllers/replyController');
 
 commentRoute.get('/fetchComments/:lodgeId', commentController.getLodgeComments);
-commentRoute.get('/fetch/:id/', commentController.getComment);
-commentRoute.put('/update/:id/', commentController.updateComment);
-commentRoute.post('/create/:lodgeId', commentController.addUserComment);
+commentRoute.get('/fetch/:id/', auth, commentController.getComment);
+commentRoute.put('/update/:id/', auth, commentController.updateComment);
+commentRoute.post('/create/:lodgeId', auth, commentController.addUserComment);
 commentRoute.delete(
   '/delete/:lodgeId/:commentId',
+  auth,
   commentController.deleteUserComment
 );
 
 // Comment Replies
-commentRoute.post('/reply/:commentId', replyController.addCommentReply);
-commentRoute.get('/reply/:replyId/', replyController.getReply);
+commentRoute.post('/reply/:commentId', auth, replyController.addCommentReply);
+commentRoute.get('/reply/:replyId/', auth, replyController.getReply);
 commentRoute.get('/replies/:commentId', replyController.getCommentReplies);
-commentRoute.put('/reply/:replyId', replyController.updateReply);
-commentRoute.delete('/reply/:commentId/:replyId', replyController.deleteCommentReply);
+commentRoute.put('/reply/:replyId', auth, replyController.updateReply);
+commentRoute.delete('/reply/:commentId/:replyId', auth, replyController.deleteCommentReply);
 
 exports.commentRoute = commentRoute;
